b11b826ae775d64a6b2a6ff183d595a34534ac97
kanwar-mana <kanwarabdullrahman@gmail.com>
2025-02-26 00:51:14 +0500

    add menu to the website

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const scroll = new LocomotiveScroll({
 var elemC= document.querySelector("#ele-container");
 var fixe=document.querySelector("#fixed-image");
 var ele = document.querySelectorAll(".element");
+
 elemC.addEventListener("mouseenter",()=>{
     fixe.style.display="block";
 })
@@ -19,6 +20,20 @@ ele.forEach(e => {
         fixe.style.backgroundImage = `url(${image_link})`;
     })
 });
+
+var menu=document.querySelector(".menu");
+var menu_anim=document.querySelector("#menu-anim");
+var flag=0;
+menu.addEventListener("click",()=>{
+    if(flag==0){
+        menu_anim.style.top=0;
+        flag=1;
+    }
+    else{
+        menu_anim.style.top="-100%";
+        flag=0;
+    }
+})
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
